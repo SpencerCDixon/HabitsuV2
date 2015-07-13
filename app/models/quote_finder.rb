dc681f7fee5fd98afdc01846b1850c0f7f6cda6f
Spencer Dixon <spencercdixon@gmail.com>
2015-07-13 18:51:25 -0400

    Refactored QuoteFinder a bit and wrote unit tests.

--- app/models/quote_finder.rb
+++ app/models/quote_finder.rb
@@ -3,10 +3,14 @@ class QuoteFinder
   # TODO: Logic for not displaying same quote to person
   # TODO: hit API to get list of lots of quotes
   def self.perform
-    if Quote.count > 0
+    if quotes_exist?
       Quote.all.sample
     else
       DefaultQuote.new("Have a kick ass day!")
     end
+  end
+
+  def self.quotes_exist?
+    Quote.count > 0
   end
 end
