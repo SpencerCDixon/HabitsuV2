032006a9b7c27deb7fd20538e3ddad7aaed1067e
Spencer Dixon <spencercdixon@gmail.com>
2015-07-13 08:32:36 -0400

    Fixed hound errors.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,17 +1,17 @@
-ENV['RAILS_ENV'] ||= 'test'
-require File.expand_path('../../config/environment', __FILE__)
+ENV["RAILS_ENV"] ||= "test"
+require File.expand_path("../../config/environment", __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
-require 'spec_helper'
-require 'rspec/rails'
-require 'shoulda-matchers'
-require File.join(File.dirname(__FILE__), 'support/valid_attribute')
-require File.join(File.dirname(__FILE__), 'support/factory_girl')
-require 'capybara/rspec'
-require 'coveralls'
-require 'webmock/rspec'
-Coveralls.wear!('rails')
+require "spec_helper"
+require "rspec/rails"
+require "shoulda-matchers"
+require File.join(File.dirname(__FILE__), "support/valid_attribute")
+require File.join(File.dirname(__FILE__), "support/factory_girl")
+require "capybara/rspec"
+require "coveralls"
+require "webmock/rspec"
+Coveralls.wear!("rails")
 
-Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 
 ActiveRecord::Migration.maintain_test_schema!
 
